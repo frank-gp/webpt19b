49d766de642d6dc9e03dfb5e0a59eddf58ed2411
Frank GP <fgp555@gmail.com>
2024-03-08 10:12:39 -0500

    add random json

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
+// Import required modules
 const express = require("express");
 const cors = require("cors");
 const tempData = require("./tempData.js");
 
+// Create an Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 const url = "https://webpt19b.web.app";
 
+// Middleware setup
 app.use(cors());
 app.use(express.static("public"));
 
+// Shuffle function using Fisher-Yates algorithm
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+// Endpoint to retrieve random movies with a specified quantity
 app.get("/movies", (req, res) => {
-  res.json(tempData);
+  const requestedQuantity = parseInt(req.query.quantity) || 12; // Default to 16 if quantity is not provided
+  const shuffledData = shuffleArray([...tempData]);
+  const selectedMovies = shuffledData.slice(0, requestedQuantity);
+  console.log(parseInt(req.query.quantity));
+  res.json(selectedMovies);
 });
 
-// Add a new endpoint to retrieve a specific movie by ID
+// Endpoint to retrieve a specific movie by ID
 app.get("/movie/:id", (req, res) => {
   const movieId = parseInt(req.params.id);
   const movie = tempData.find((m) => m.id === movieId);
@@ -30,6 +47,7 @@ app.get("*", (req, res) => {
   res.redirect(url);
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
 });
