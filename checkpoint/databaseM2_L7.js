74b4509eeecda72b1ab08158b6a63ea63c27fa4a
Frank GP <fgp555@gmail.com>
2024-03-25 11:27:59 -0500

    add databaseM2_07

--- checkpoint/databaseM2_L7.js
+++ checkpoint/databaseM2_L7.js
@@ -1,4 +1,4 @@
-// databaseM2_L6.js
+// data.js
 
 module.exports = [
   {
@@ -14,4 +14,84 @@ module.exports = [
     categoria: "Modulo 2: L-07 HTTP",
     seccion: "01 Estructura básica del servidors",
   },
+  {
+    id: 2,
+    pregunta: "¿Qué representa el 'localhost' y el puerto en el contexto de las aplicaciones web?",
+    opciones: [
+      "El 'localhost' indica la dirección IP del servidor y el puerto especifica la aplicación en particular",
+      "El 'localhost' es el nombre de dominio de un servidor y el puerto es la dirección física del mismo",
+      "El 'localhost' es la dirección del cliente y el puerto indica la dirección del servidor",
+      "El 'localhost' es la dirección del servidor y el puerto especifica la puerta de entrada a una aplicación en ese servidor",
+    ],
+    respuestaCorrecta: "El 'localhost' es la dirección del servidor y el puerto especifica la puerta de entrada a una aplicación en ese servidor",
+    categoria: "Modulo 2: L-07 HTTP",
+    seccion: "01 Estructura básica del servidor",
+  },
+  {
+    id: 3,
+    pregunta: "¿Cuál es la analogía propuesta para entender el funcionamiento de los controladores en un servidor web?",
+    opciones: [
+      "El chef en acción en una cocina, recibiendo pedidos y preparando platos",
+      "El mecánico reparando un automóvil en un taller",
+      "El capitán de un barco dirigiendo la nave en alta mar",
+      "El conductor de un autobús llevando pasajeros a su destino",
+    ],
+    respuestaCorrecta: "El chef en acción en una cocina, recibiendo pedidos y preparando platos",
+    categoria: "Modulo 2: L-07 HTTP",
+    seccion: "01 Estructura básica del servidor",
+  },
+  {
+    id: 4,
+    pregunta: "¿Cómo se comparan los servicios en un servidor web con el personal en un restaurante?",
+    opciones: [
+      "Los servicios son como un equipo coordinado de meseros, cada uno especializado en un área particular",
+      "Los servicios son como el chef en la cocina, preparando los platos según los pedidos",
+      "Los servicios son como los clientes en el restaurante, solicitando diferentes platos",
+      "Los servicios son como los ingredientes en la despensa, utilizados para preparar los platos",
+    ],
+    respuestaCorrecta: "Los servicios son como un equipo coordinado de meseros, cada uno especializado en un área particular",
+    categoria: "Modulo 2: L-07 HTTP",
+    seccion: "01 Estructura básica del servidor",
+  },
+  {
+    id: 5,
+    pregunta: "¿Cuál es la función de los middlewares en un servidor web?",
+    opciones: [
+      "Mejorar o darle un toque final al resultado antes de que sea enviado al cliente",
+      "Encargarse de recibir y procesar las solicitudes de los clientes",
+      "Gestionar la autenticación y autorización de usuarios",
+      "Interactuar con la base de datos para recuperar o almacenar información",
+    ],
+    respuestaCorrecta: "Mejorar o darle un toque final al resultado antes de que sea enviado al cliente",
+    categoria: "Modulo 2: L-07 HTTP",
+    seccion: "01 Estructura básica del servidor",
+  },
+  {
+    id: 6,
+    pregunta: "¿Cuál es el propósito principal del scaffolding de proyecto en el desarrollo web?",
+    opciones: [
+      "Crear una estructura de carpetas para organizar el código de manera eficiente",
+      "Ejecutar pruebas automáticas en el código",
+      "Optimizar el rendimiento del servidor",
+      "Implementar funciones de seguridad en el proyecto",
+    ],
+    respuestaCorrecta: "Crear una estructura de carpetas para organizar el código de manera eficiente",
+    categoria: "Modulo 2: L-07 HTTP",
+    seccion: "02 Scaffolding de proyecto",
+  },
+  {
+    id: 7,
+    pregunta: "¿Cuáles son las carpetas comúnmente utilizadas en el contexto de desarrollo web y patrones de diseño MVC para organizar y estructurar el código?",
+    opciones: [
+      "Rutas y Controladores",
+      "Vistas y Modelos",
+      "Clientes y Servidores",
+      "CSS y JS",
+    ],
+    respuestaCorrecta: "Rutas y Controladores",
+    categoria: "Modulo 2: L-07 HTTP",
+    seccion: "02 Scaffolding de proyecto",
+  }
+  
+  
 ];
