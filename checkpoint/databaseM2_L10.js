9754552663493dedef7be480bd64804fb4f6334c
vanesabucher <buchervanesa@gmail.com>
2024-04-16 11:34:26 -0300

    l10 y l11

--- checkpoint/databaseM2_L10.js
+++ checkpoint/databaseM2_L10.js
@@ -1,288 +1,156 @@
-module.exports = [{
-    "id": 501,
-    "pregunta": "¿Cuál es una característica de las bases de datos NoSQL?",
-    "opciones": [
+module.exports = [
+  {
+    id: 1001,
+    pregunta: "¿Cuál es una característica de las bases de datos NoSQL?",
+    opciones: [
       "No utilizan SQL como lenguaje de consulta",
       "Son exclusivamente bases de datos en la nube",
       "Utilizan exclusivamente SQL como lenguaje de consulta",
-      "No permiten consultas complejas"
+      "No permiten consultas complejas",
     ],
-    "respuestaCorrecta": "No utilizan SQL como lenguaje de consulta",
-    "categoria": "Módulo 2: L10 MongoDb",
-    "seccion": "02.01 MongoDb"
+    respuestaCorrecta: "No utilizan SQL como lenguaje de consulta",
+    categoria: "Módulo 2: L10 MongoDb",
+    seccion: "01 MongoDb",
   },
 
   {
-    "id": 502,
-    "pregunta": "¿Cómo almacena MongoDB los datos?",
-    "opciones": [
+    id: 1002,
+    pregunta: "¿Cómo almacena MongoDB los datos?",
+    opciones: [
       "En tablas",
       "En documentos",
       "En archivos de texto plano",
-      "En mapas"
+      "En mapas",
     ],
-    "respuestaCorrecta": "En documentos",
-    "categoria": "Módulo 2: L10 MongoDb",
-    "seccion": "02.01 MongoDb"
+    respuestaCorrecta: "En documentos",
+    categoria: "Módulo 2: L10 MongoDb",
+    seccion: "01 MongoDb",
   },
   {
-    "id": 503,
-    "pregunta": "¿Cuál es una característica clave de MongoDB?",
-    "opciones": [
+    id: 1003,
+    pregunta: "¿Cuál es una característica clave de MongoDB?",
+    opciones: [
       "Utiliza un esquema dinámico",
       "Utiliza un esquema estático",
       "Utiliza un esquema relacional",
-      "Utiliza un esquema en forma de tablas"
+      "Utiliza un esquema en forma de tablas",
     ],
-    "respuestaCorrecta": "Utiliza un esquema dinámico",
-    "categoria": "Módulo 2: L10 MongoDb",
-    "seccion": "02.01 MongoDb"
+    respuestaCorrecta: "Utiliza un esquema dinámico",
+    categoria: "Módulo 2: L10 MongoDb",
+    seccion: "01 MongoDb",
   },
   {
-    "id": 504,
-    "pregunta": "¿Qué es BSON y para qué se utiliza en MongoDB?",
-    "opciones": [
+    id: 1004,
+    pregunta: "¿Qué es BSON y para qué se utiliza en MongoDB?",
+    opciones: [
       "Es un formato de datos legible por humanos",
       "Es un formato creado por MongoDB para mejorar la eficiencia en la transferencia de datos",
       "Es un formato exclusivo de bases de datos SQL",
-      "Es un formato para almacenar datos en tablas"
+      "Es un formato para almacenar datos en tablas",
     ],
-    "respuestaCorrecta": "Es un formato creado por MongoDB para mejorar la eficiencia en la transferencia de datos",
-    "categoria": "Modulo 2: L10 MongoDb",
-    "seccion": "02.01 MongoDb"
+    respuestaCorrecta:
+      "Es un formato creado por MongoDB para mejorar la eficiencia en la transferencia de datos",
+    categoria: "Modulo 2: L10 MongoDb",
+    seccion: "01 MongoDb",
   },
   {
-    "id": 505,
-    "pregunta": "¿Qué es un documento en MongoDB?",
-    "opciones": [
+    id: 1005,
+    pregunta: "¿Qué es un documento en MongoDB?",
+    opciones: [
       "Es un formato de datos legible por humanos",
       "Es un formato creado por MongoDB para mejorar la eficiencia en la transferencia de datos",
       "Es la unidad básica de almacenamiento, representada en formato BSON y contiene pares de campos (clave-valor)",
-      "Es una tabla que contiene datos relacionados"
+      "Es una tabla que contiene datos relacionados",
     ],
-    "respuestaCorrecta": "Es la unidad básica de almacenamiento, representada en formato BSON y contiene pares de campos (clave-valor)",
-    "categoria": "Modulo 2: L10 MongoDb",
-    "seccion": "02.02 MongoDb"
+    respuestaCorrecta:
+      "Es la unidad básica de almacenamiento, representada en formato BSON y contiene pares de campos (clave-valor)",
+    categoria: "Modulo 2: L10 MongoDb",
+    seccion: "02 MongoDb",
   },
   {
-    "id": 506,
-    "pregunta": "¿Qué es una colección en MongoDB?",
-    "opciones": [
+    id: 1006,
+    pregunta: "¿Qué es una colección en MongoDB?",
+    opciones: [
       "Es un formato de datos legible por humanos",
       "Es un formato creado por MongoDB para mejorar la eficiencia en la transferencia de datos",
       "Es un conjunto de documentos almacenados que se encuentran relacionados entre sí",
-      "Es una tabla que contiene datos relacionados"
+      "Es una tabla que contiene datos relacionados",
     ],
-    "respuestaCorrecta": "Es un conjunto de documentos almacenados que se encuentran relacionados entre sí",
-    "categoria": "Modulo 2: L10 MongoDb",
-    "seccion": "02.02 MongoDb"
+    respuestaCorrecta:
+      "Es un conjunto de documentos almacenados que se encuentran relacionados entre sí",
+    categoria: "Modulo 2: L10 MongoDb",
+    seccion: "02 MongoDb",
   },
   {
-    "id": 507,
-    "pregunta": "¿Qué son las referencias entre documentos en MongoDB?",
-    "opciones": [
+    id: 1007,
+    pregunta: "¿Qué son las referencias entre documentos en MongoDB?",
+    opciones: [
       "Es un formato de datos legible por humanos",
       "Es un tipo de conexión entre dos documentos que permite que uno de ellos pueda abarcar la información del otro",
       "Es un formato específico de bases de datos relacionales",
-      "Es una forma de almacenar datos en tablas"
+      "Es una forma de almacenar datos en tablas",
     ],
-    "respuestaCorrecta": "Es un tipo de conexión entre dos documentos que permite que uno de ellos pueda abarcar la información del otro",
-    "categoria": "Modulo 2: L10 MongoDb",
-    "seccion": "02.02 MongoDb"
+    respuestaCorrecta:
+      "Es un tipo de conexión entre dos documentos que permite que uno de ellos pueda abarcar la información del otro",
+    categoria: "Modulo 2: L10 MongoDb",
+    seccion: "02 MongoDb",
   },
   {
-    "id": 508,
-    "pregunta": "¿Qué es MongoDB Atlas?",
-    "opciones": [
+    id: 1008,
+    pregunta: "¿Qué es MongoDB Atlas?",
+    opciones: [
       "Es un formato de datos legible por humanos",
       "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
       "Es un tipo de conexión entre dos documentos en MongoDB",
-      "Es una colección de documentos relacionados en MongoDB"
+      "Es una colección de documentos relacionados en MongoDB",
     ],
-    "respuestaCorrecta": "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
-    "categoria": "Modulo 2: L10 MongoDb",
-    "seccion": "02.03 MongoDb"
+    respuestaCorrecta:
+      "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
+    categoria: "Modulo 2: L10 MongoDb",
+    seccion: "03 MongoDb",
   },
   {
-    "id": 509,
-    "pregunta": "¿Cuáles son algunas características fundamentales de MongoDB Atlas?",
-    "opciones": [
+    id: 1009,
+    pregunta:
+      "¿Cuáles son algunas características fundamentales de MongoDB Atlas?",
+    opciones: [
       "Permite deployar de manera sencilla nuestra base de datos",
       "Ofrece la capacidad de escalamiento horizontal de manera automática",
       "Realiza respaldos automáticos regulares",
       "Permite la recuperación de datos en caso de pérdida o errores",
-      "Todas las anteriores"
+      "Todas las anteriores",
     ],
-    "respuestaCorrecta": "Todas las anteriores",
-    "categoria": "Modulo 2: L10 MongoDb",
-    "seccion": "02.03 MongoDb"
+    respuestaCorrecta: "Todas las anteriores",
+    categoria: "Modulo 2: L10 MongoDb",
+    seccion: "03 MongoDb",
   },
   {
-    "id": 510,
-    "pregunta": "¿Qué es MongoDB Compass?",
-    "opciones": [
+    id: 1010,
+    pregunta: "¿Qué es MongoDB Compass?",
+    opciones: [
       "Es un formato de datos legible por humanos",
       "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
       "Es una interfaz mediante la cuál podremos trabajar con la base de datos de la nube en MongoDB",
-      "Es una herramienta para crear referencias entre documentos en MongoDB"
+      "Es una herramienta para crear referencias entre documentos en MongoDB",
     ],
-    "respuestaCorrecta": "Es una interfaz mediante la cuál podremos trabajar con la base de datos de la nube en MongoDB",
-    "categoria": "Modulo 2: L10 MongoDb",
-    "seccion": "02.03 MongoDb"
+    respuestaCorrecta:
+      "Es una interfaz mediante la cuál podremos trabajar con la base de datos de la nube en MongoDB",
+    categoria: "Modulo 2: L10 MongoDb",
+    seccion: "03 MongoDb",
   },
   {
-    "id": 511,
-    "pregunta": "¿Por qué usar MongoDB Compass en lugar de trabajar directamente desde MongoDB Atlas?",
-    "opciones": [
+    id: 1011,
+    pregunta:
+      "¿Por qué usar MongoDB Compass en lugar de trabajar directamente desde MongoDB Atlas?",
+    opciones: [
       "Porque Compass utiliza muchos menos recursos",
       "Porque Compass es una aplicación local y no necesita que entremos en el navegador",
       "Porque Compass ofrece una interfaz más intuitiva y fácil de usar que MongoDB Atlas",
-      "Todas las anteriores"
+      "Todas las anteriores",
     ],
-    "respuestaCorrecta": "Todas las anteriores",
-    "categoria": "Modulo 2: L10 MongoDb",
-    "seccion": "02.03 MongoDb"
+    respuestaCorrecta: "Todas las anteriores",
+    categoria: "Modulo 2: L10 MongoDb",
+    seccion: "03 MongoDb",
   },
-  {
-    "id": 513,
-    "pregunta": "¿Qué son los ODM (Object Data Modeling) y cuál es su función principal?",
-    "opciones": [
-      "Son librerías que permiten manipular y convertir datos entre dos sistemas",
-      "Son librerías que permiten trabajar con bases de datos relacionales y no relacionales",
-      "Son librerías que permiten trabajar con datos en formato JSON y BSON",
-      "Son librerías que permiten manipular y convertir datos incompatibles naturalmente entre dos sistemas"
-    ],
-    "respuestaCorrecta": "Son librerías que permiten manipular y convertir datos incompatibles naturalmente entre dos sistemas",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 514,
-    "pregunta": "¿Qué es Mongoose en el contexto de MongoDB?",
-    "opciones": [
-      "Es un formato de datos legible por humanos",
-      "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
-      "Es una librería ODM específica para MongoDB que permite interactuar con la base de datos desde el código",
-      "Es una herramienta para crear referencias entre documentos en MongoDB"
-    ],
-    "respuestaCorrecta": "Es una librería ODM específica para MongoDB que permite interactuar con la base de datos desde el código",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 515,
-    "pregunta": "¿Para qué tipo de desarrollo está diseñada la biblioteca Mongoose?",
-    "opciones": [
-      "Para el modelado de objetos de MongoDB con Java",
-      "Para el modelado de objetos de MongoDB con Python",
-      "Para el modelado de objetos de MongoDB con JavaScript o TypeScript",
-      "Para el modelado de objetos de MongoDB con C#"
-    ],
-    "respuestaCorrecta": "Para el modelado de objetos de MongoDB con JavaScript o TypeScript",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 516,
-    "pregunta": "¿Qué es un schema en Mongoose?",
-    "opciones": [
-      "Es un formato de datos legible por humanos",
-      "Es un objeto que lleva la configuración de una colección en MongoDB",
-      "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
-      "Es una herramienta para crear referencias entre documentos en MongoDB"
-    ],
-    "respuestaCorrecta": "Es un objeto que lleva la configuración de una colección en MongoDB",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 517,
-    "pregunta": "¿Qué tipo de información se puede configurar en un schema de Mongoose?",
-    "opciones": [
-      "Nombre, raza, color y edad de un perro",
-      "Nombre, apellido, edad y dirección de una persona",
-      "Marca, modelo, año y color de un automóvil",
-      "Todas las anteriores"
-    ],
-    "respuestaCorrecta": "Todas las anteriores",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 518,
-    "pregunta": "¿Qué significa tipar los datos en un schema de Mongoose?",
-    "opciones": [
-      "Definir el tipo de datos que vamos a guardar en cada propiedad de un documento",
-      "Definir la estructura de un documento en MongoDB",
-      "Definir una relación entre dos documentos en MongoDB",
-      "Definir una colección en MongoDB"
-    ],
-    "respuestaCorrecta": "Definir el tipo de datos que vamos a guardar en cada propiedad de un documento",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 519,
-    "pregunta": "¿Qué es un modelo en Mongoose?",
-    "opciones": [
-      "Es un objeto basado en un schema que nos permite interactuar con una colección específica en MongoDB",
-      "Es un formato de datos legible por humanos",
-      "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
-      "Es una herramienta para crear referencias entre documentos en MongoDB"
-    ],
-    "respuestaCorrecta": "Es un objeto basado en un schema que nos permite interactuar con una colección específica en MongoDB",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 520,
-    "pregunta": "¿Cómo podemos declarar un modelo a partir de un schema en Mongoose?",
-    "opciones": [
-      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'model' de mongoose con el nombre del modelo y el schema como argumentos",
-      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'schema' de mongoose con el nombre del modelo y el schema como argumentos",
-      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'createModel' de mongoose con el nombre del modelo y el schema como argumentos",
-      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'createSchema' de mongoose con el nombre del modelo y el schema como argumentos"
-    ],
-    "respuestaCorrecta": "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'model' de mongoose con el nombre del modelo y el schema como argumentos",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 521,
-    "pregunta": "¿Cómo describirías la relación entre un schema y un modelo en Mongoose?",
-    "opciones": [
-      "Son estructuras completamente independientes una de la otra",
-      "Son estructuras que no tienen relación entre sí",
-      "Son estructuras diferentes pero complementarias entre sí, con una relación simbiótica donde una depende de la otra",
-      "Son estructuras que comparten la misma funcionalidad y propósito"
-    ],
-    "respuestaCorrecta": "Son estructuras diferentes pero complementarias entre sí, con una relación simbiótica donde una depende de la otra",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 522,
-    "pregunta": "¿Qué define el schema en Mongoose?",
-    "opciones": [
-      "Define la estructura de los documentos en una colección",
-      "Define la estructura de un modelo en Mongoose",
-      "Define la estructura de una base de datos en MongoDB",
-      "Define la estructura de una consulta en Mongoose"
-    ],
-    "respuestaCorrecta": "Define la estructura de los documentos en una colección",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  },
-  {
-    "id": 523,
-    "pregunta": "¿Cómo utiliza el modelo el schema en Mongoose?",
-    "opciones": [
-      "Para definir la estructura de los documentos en una colección",
-      "Para proporcionar un objeto con el que podremos interactuar con la base de datos",
-      "Para definir la estructura de un modelo en Mongoose",
-      "Para definir la estructura de una consulta en Mongoose"
-    ],
-    "respuestaCorrecta": "Para proporcionar un objeto con el que podremos interactuar con la base de datos",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
-  }];
+  
+];
