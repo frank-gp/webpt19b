2ca909b17a7f2ced37017b682d8529d12757846b
vanesabucher <buchervanesa@gmail.com>
2024-04-16 11:34:26 -0300

    l10 y l11

--- checkpoint/databaseM2_L11.js
+++ checkpoint/databaseM2_L11.js
@@ -1,16 +1,175 @@
-module.exports = [{
-    "id": 512,
-    "pregunta": "¿Qué tecnologías se utilizan para desarrollar la funcionalidad de un servidor y la base de datos en MongoDB?",
-    "opciones": [
+module.exports = [
+  {
+    id: 1101,
+    pregunta:
+      "¿Qué tecnologías se utilizan para desarrollar la funcionalidad de un servidor y la base de datos en MongoDB?",
+    opciones: [
       "JavaScript para el servidor y formato BSON para la base de datos",
       "JavaScript para el servidor y formato JSON para la base de datos",
       "PHP para el servidor y formato BSON para la base de datos",
-      "PHP para el servidor y formato JSON para la base de datos"
+      "PHP para el servidor y formato JSON para la base de datos",
     ],
-    "respuestaCorrecta": "JavaScript para el servidor y formato BSON para la base de datos",
-    "categoria": "Modulo 2: L11 MongoDb",
-    "seccion": "11.01 MongoDb"
+    respuestaCorrecta:
+      "JavaScript para el servidor y formato BSON para la base de datos",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
   },
-
-  
-  ];
+  {
+    id: 1102,
+    pregunta:
+      "¿Qué son los ODM (Object Data Modeling) y cuál es su función principal?",
+    opciones: [
+      "Son librerías que permiten manipular y convertir datos entre dos sistemas",
+      "Son librerías que permiten trabajar con bases de datos relacionales y no relacionales",
+      "Son librerías que permiten trabajar con datos en formato JSON y BSON",
+      "Son librerías que permiten manipular y convertir datos incompatibles naturalmente entre dos sistemas",
+    ],
+    respuestaCorrecta:
+      "Son librerías que permiten manipular y convertir datos incompatibles naturalmente entre dos sistemas",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1103,
+    pregunta: "¿Qué es Mongoose en el contexto de MongoDB?",
+    opciones: [
+      "Es un formato de datos legible por humanos",
+      "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
+      "Es una librería ODM específica para MongoDB que permite interactuar con la base de datos desde el código",
+      "Es una herramienta para crear referencias entre documentos en MongoDB",
+    ],
+    respuestaCorrecta:
+      "Es una librería ODM específica para MongoDB que permite interactuar con la base de datos desde el código",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1104,
+    pregunta:
+      "¿Para qué tipo de desarrollo está diseñada la biblioteca Mongoose?",
+    opciones: [
+      "Para el modelado de objetos de MongoDB con Java",
+      "Para el modelado de objetos de MongoDB con Python",
+      "Para el modelado de objetos de MongoDB con JavaScript o TypeScript",
+      "Para el modelado de objetos de MongoDB con C#",
+    ],
+    respuestaCorrecta:
+      "Para el modelado de objetos de MongoDB con JavaScript o TypeScript",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1105,
+    pregunta: "¿Qué es un schema en Mongoose?",
+    opciones: [
+      "Es un formato de datos legible por humanos",
+      "Es un objeto que lleva la configuración de una colección en MongoDB",
+      "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
+      "Es una herramienta para crear referencias entre documentos en MongoDB",
+    ],
+    respuestaCorrecta:
+      "Es un objeto que lleva la configuración de una colección en MongoDB",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1106,
+    pregunta:
+      "¿Qué tipo de información se puede configurar en un schema de Mongoose?",
+    opciones: [
+      "Nombre, raza, color y edad de un perro",
+      "Nombre, apellido, edad y dirección de una persona",
+      "Marca, modelo, año y color de un automóvil",
+      "Todas las anteriores",
+    ],
+    respuestaCorrecta: "Todas las anteriores",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1107,
+    pregunta: "¿Qué significa tipar los datos en un schema de Mongoose?",
+    opciones: [
+      "Definir el tipo de datos que vamos a guardar en cada propiedad de un documento",
+      "Definir la estructura de un documento en MongoDB",
+      "Definir una relación entre dos documentos en MongoDB",
+      "Definir una colección en MongoDB",
+    ],
+    respuestaCorrecta:
+      "Definir el tipo de datos que vamos a guardar en cada propiedad de un documento",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1108,
+    pregunta: "¿Qué es un modelo en Mongoose?",
+    opciones: [
+      "Es un objeto basado en un schema que nos permite interactuar con una colección específica en MongoDB",
+      "Es un formato de datos legible por humanos",
+      "Es una plataforma de base de datos en la nube ofrecida por MongoDB",
+      "Es una herramienta para crear referencias entre documentos en MongoDB",
+    ],
+    respuestaCorrecta:
+      "Es un objeto basado en un schema que nos permite interactuar con una colección específica en MongoDB",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1109,
+    pregunta:
+      "¿Cómo podemos declarar un modelo a partir de un schema en Mongoose?",
+    opciones: [
+      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'model' de mongoose con el nombre del modelo y el schema como argumentos",
+      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'schema' de mongoose con el nombre del modelo y el schema como argumentos",
+      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'createModel' de mongoose con el nombre del modelo y el schema como argumentos",
+      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'createSchema' de mongoose con el nombre del modelo y el schema como argumentos",
+    ],
+    respuestaCorrecta:
+      "Crear una variable llamada 'User' y asignarle el resultado de llamar al método 'model' de mongoose con el nombre del modelo y el schema como argumentos",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1110,
+    pregunta:
+      "¿Cómo describirías la relación entre un schema y un modelo en Mongoose?",
+    opciones: [
+      "Son estructuras completamente independientes una de la otra",
+      "Son estructuras que no tienen relación entre sí",
+      "Son estructuras diferentes pero complementarias entre sí, con una relación simbiótica donde una depende de la otra",
+      "Son estructuras que comparten la misma funcionalidad y propósito",
+    ],
+    respuestaCorrecta:
+      "Son estructuras diferentes pero complementarias entre sí, con una relación simbiótica donde una depende de la otra",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1111,
+    pregunta: "¿Qué define el schema en Mongoose?",
+    opciones: [
+      "Define la estructura de los documentos en una colección",
+      "Define la estructura de un modelo en Mongoose",
+      "Define la estructura de una base de datos en MongoDB",
+      "Define la estructura de una consulta en Mongoose",
+    ],
+    respuestaCorrecta:
+      "Define la estructura de los documentos en una colección",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+  {
+    id: 1112,
+    pregunta: "¿Cómo utiliza el modelo el schema en Mongoose?",
+    opciones: [
+      "Para definir la estructura de los documentos en una colección",
+      "Para proporcionar un objeto con el que podremos interactuar con la base de datos",
+      "Para definir la estructura de un modelo en Mongoose",
+      "Para definir la estructura de una consulta en Mongoose",
+    ],
+    respuestaCorrecta:
+      "Para proporcionar un objeto con el que podremos interactuar con la base de datos",
+    categoria: "Modulo 2: L11 MongoDb",
+    seccion: "01 Mongoose",
+  },
+];
